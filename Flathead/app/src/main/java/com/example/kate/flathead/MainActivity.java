24bcfcdb2bf0134273374a7b3a1074dac41058d9
Kate Twiss <karaden@gmail.com>
2017-11-20 09:53:40 +0100

    Added in image, and ability to turn image on or off

--- Flathead/app/src/main/java/com/example/kate/flathead/MainActivity.java
+++ Flathead/app/src/main/java/com/example/kate/flathead/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,10 +14,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RadioButton message01, message02, message03;
+    private RadioButton message01, message02, message03, message04;
     private Button btnDisplay;
     private TextView label;
     private Typeface furmanite, datacontrol;
+    private ImageView logo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
         label = findViewById(R.id.chosenLabel);
         addListenerOnButton();
+
+        logo = findViewById(R.id.logo);
+        logo.setImageResource(R.drawable.functionistcouncilinsignia);
+        logo.setVisibility(View.INVISIBLE);
 
         furmanite = Typeface.createFromAsset(getAssets(), "fonts/furmanite.otf");
         datacontrol = Typeface.createFromAsset(getAssets(), "fonts/datacontrol.ttf");
@@ -44,18 +50,31 @@ public class MainActivity extends AppCompatActivity {
                 if (checked) {
                     label.setText(R.string.message01);
                     label.setTypeface(furmanite);
+
+                    logo.setVisibility(View.INVISIBLE);
                 }
                 break;
             case R.id.message02:
                 if (checked) {
                     label.setText(R.string.message02);
                     label.setTypeface(datacontrol);
+
+                    logo.setVisibility(View.INVISIBLE);
                 }
                 break;
             case R.id.message03:
                 if (checked) {
                     label.setText(R.string.message03);
                     label.setTypeface(Typeface.DEFAULT);
+
+                    logo.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.message04:
+                if (checked) {
+                    label.setText("");
+
+                    logo.setVisibility(View.VISIBLE);
                 }
                 break;
         }
@@ -67,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         message01 = findViewById(R.id.message01);
         message02 = findViewById(R.id.message02);
         message03 = findViewById(R.id.message03);
+        message04 = findViewById(R.id.message04);
         btnDisplay = findViewById(R.id.btnDisplay);
 
         btnDisplay.setOnClickListener(new OnClickListener() {
@@ -78,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 String result = "Message 1 check : " + message01.isChecked() +
                         "\nMessage 2 check : " + message02.isChecked() +
                         "\nMessage 3 check : " + message03.isChecked() +
+                        "\nMessage 4 check : " + message04.isChecked() +
                         "\n Typeface is: " + label.getTypeface().toString();
 
                 Toast.makeText(MainActivity.this, result,
